011b2f10d8bf51feb827222ef745d7bc2245209a
Igor Sysoev <igro.sia@yandex.ru>
2023-04-15 12:12:06 +0300

    add sing-in

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -26,6 +26,26 @@ func (h *Handler) singUp(c *gin.Context) {
 	})
 }
 
-func (h *Handler) singIn(c *gin.Context) {
+type singInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
+func (h *Handler) singIn(c *gin.Context) {
+	var input singInInput
+
+	//parse input
+	if err := c.BindJSON(&input); err != nil {
+		newErrorRespons(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorRespons(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
--- pkg/servise/auth.go
+++ pkg/servise/auth.go
@@ -3,11 +3,23 @@ package servise
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 	"todo"
 	"todo/pkg/repository"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "jdskds323ej"
+const (
+	salt       = "jdskds323ej"
+	signingKey = "kdskdskdks2131kkk"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,6 +32,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generationPasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generationPasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+	// create token custom struct, use userID
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(12 * time.Hour).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generationPasswordHash(password string) string {
